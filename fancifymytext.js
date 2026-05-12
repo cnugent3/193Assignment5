983fb5fc0f23b117c63b7fec59bf8415400bb613
cnugent3 <cnugent773@gmail.com>
2026-05-12 15:43:54 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -1,4 +1,13 @@
 function makeBigger() {
   alert("Button Clicked!");
   document.getElementById("textInput").style.fontSize = "24pt";
+  }
+function changeStyle() {
+  alert("Radio button changed!");
+
+  if (document.getElementById("fancyShmancy").checked) {
+    document.getElementById("textInput").style.fontWeight = "bold";
+  } else if (document.getElementById("boringBetty").checked) {
+    document.getElementById("textInput").style.fontWeight = "normal";
+  }
 }
