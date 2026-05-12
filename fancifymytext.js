1a53edc3af0064ce55f823b38f5de46e8beddbd3
cnugent3 <cnugent773@gmail.com>
2026-05-12 16:02:09 -0700

    Update fancifymytext.js

--- fancifymytext.js
+++ fancifymytext.js
@@ -6,20 +6,21 @@ function changeStyle() {
   alert("Radio button changed!");
 
   if (document.getElementById("fancyShmancy").checked) {
-    document.getElementById("textInput").style.fontWeight = "bold";
-    document.getElementById("textInput").style.color = "blue";
-    document.getElementById("textInput").style.textDecoration = "underline";
+      document.getElementById("textInput").style.fontWeight = "bold";
+      document.getElementById("textInput").style.color = "blue";
+      document.getElementById("textInput").style.textDecoration = "underline";
   } else if (document.getElementById("boringBetty").checked) {
-    document.getElementById("textInput").style.fontWeight = "normal";
-    document.getElementById("textInput").style.color = "black";
-    document.getElementById("textInput").style.textDecoration = "none";
+      document.getElementById("textInput").style.fontWeight = "normal";
+      document.getElementById("textInput").style.color = "black";
+      document.getElementById("textInput").style.textDecoration = "none";
   }
-  function mooText() {
-  var str = document.getElementById("textInput");
-  var textUpper = str.value;
-  textUpper = textUpper.toUpperCase();
-  var parts = textUpper.split(".");
-  textUpper = parts.join("-Moo.");
-  str.value = textUpper;
+function mooText() {
+    alert("Moo Clicked! Text Changed!");
+    var str = document.getElementById("textInput");
+    var textUpper = str.value;
+    textUpper = textUpper.toUpperCase();
+    var parts = textUpper.split(".");
+    textUpper = parts.join("-Moo.");
+    str.value = textUpper;
 }
 }
